38474d4b085f0cbfe1a415f165c549bf6981ece6
Querijn Heijmans <querijn@irule.at>
2017-06-17 18:22:56 +0200

    Moved external files to separate folder.

--- js/settings.js
+++ js/settings.js
@@ -3,6 +3,6 @@ var Minesweeper = Minesweeper || {};
 Minesweeper.Settings =
 {
     Mines: { Min: 0.1, Max: 0.33, Current: 0.33 },
-    Tiles: { Min: 5, Max: 60, Current: 9 }
+    Tiles: { Min: 5, Max: 60, Current: 9 },
     Zoom: { Min: 0.5, Max: 2.0 }
 };
